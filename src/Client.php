747472706e39d5686aad1f2db881c2bf561a6cd7
Sullivan SENECHAL <sullivan@nexylan.com>
2015-06-23 11:26:40 +0200

    OptionsResolver with default options

--- src/Client.php
+++ src/Client.php
@@ -11,9 +11,38 @@
 
 namespace Nexy\CloudFlare;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 /**
  * @author Sullivan Senechal <soullivaneuh@gmail.com>
  */
 class Client
 {
+    const API_BASE_URL = 'https://api.cloudflare.com/client/v4';
+
+    /**
+     * @var array
+     */
+    private $options;
+
+    /**
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+
+        $this->options = $resolver->resolve($options);
+    }
+
+    private function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'user_agent'    => 'nexylan-cloudflare-sdk (https://github.com/nexylan/cloudflare)',
+            'timeout'       => 10,
+        ]);
+        $resolver->setAllowedTypes('user_agent', 'string');
+        $resolver->setAllowedTypes('timeout', 'int');
+    }
 }
