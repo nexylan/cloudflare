d21f661b580d11fce2fddfa3556adfd26fdab10e
Sullivan SENECHAL <sullivan@nexylan.com>
2015-07-03 13:08:31 +0200

    NexyCloudFlareBundle

---
+++ src/Bundle/NexyCloudFlareBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Nexylan CloudFlare package.
+ *
+ * (c) Nexylan SAS <contact@nexylan.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nexy\CloudFlare\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Sullivan Senechal <soullivaneuh@gmail.com>
+ */
+class NexyCloudFlareBundle extends Bundle
+{
+}
